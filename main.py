d523c7a2a62d27683e74b1a9362ddaf44fa9a4ec
Arthur Lee <ruhtraeel@gmail.com>
2019-10-21 17:01:32 -0700

    Got initial accuracy number

--- main.py
+++ main.py
@@ -1,9 +1,79 @@
 import pandas as pd
 import numpy as np
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 from string import punctuation
 from collections import Counter
+
+class SentimentRNN(nn.Module):
+    """
+    The RNN model that will be used to perform Sentiment analysis.
+    """
+
+    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, drop_prob=0.5):
+        """
+        Initialize the model by setting up the layers.
+        """
+        super(SentimentRNN, self).__init__()
+
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+
+        # embedding and LSTM layers
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers,
+                            dropout=drop_prob, batch_first=True)
+
+        # dropout layer
+        self.dropout = nn.Dropout(0.3)
+
+        # linear and sigmoid layers
+        self.fc = nn.Linear(hidden_dim, output_size)
+        self.sig = nn.Sigmoid()
+
+        # define all layers
+
+
+    def forward(self, x, hidden):
+        """
+        Perform a forward pass of our model on some input and hidden state.
+        """
+        batch_size = x.size(0)
+
+        # embeddings and lstm_out
+        x = x.long()
+        embeds = self.embedding(x)
+        lstm_out, hidden = self.lstm(embeds, hidden)
+
+        # stack up lstm outputs
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+
+        # dropout and fully-connected layer
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        # sigmoid function
+        sig_out = self.sig(out)
+
+        # reshape to be batch_size first
+        sig_out = sig_out.view(batch_size, -1)
+        sig_out = sig_out[:, -1] # get last batch of labels
+
+        # return last sigmoid output and hidden state
+        return sig_out, hidden
+
+
+    def init_hidden(self, batch_size):
+        ''' Initializes hidden state '''
+        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
+        # initialized to zero, for hidden state and cell state of LSTM
+        weight = next(self.parameters()).data
+
+        hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+                      weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+
+        return hidden
 
 if __name__ == '__main__':
     df = pd.DataFrame.from_csv('testdata.csv')
@@ -86,7 +156,7 @@ if __name__ == '__main__':
     valid_data = TensorDataset(torch.from_numpy(valid_x), torch.from_numpy(valid_y))
     test_data = TensorDataset(torch.from_numpy(test_x), torch.from_numpy(test_y))
     # dataloaders
-    batch_size = 50
+    batch_size = 10
     # make sure to SHUFFLE your data
     train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
     valid_loader = DataLoader(valid_data, shuffle=True, batch_size=batch_size)
@@ -109,12 +179,55 @@ if __name__ == '__main__':
     n_layers = 2
 
     #TODO: Implement below
-    net = SentimentLSTM(vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
+    net = SentimentRNN(vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
     print(net)
-    SentimentRNN(
-        Embedding(74073, 400),
-        LSTM(400, 256, num_layers=2, batch_first=True, dropout=0.5),
-        Dropout(p=0.3),
-        Linear(in_features=256, out_features=1, bias=True),
-        Sigmoid()
-    )
+
+    # loss and optimization functions
+    lr=0.001
+
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+
+    net.train()
+
+    # Get test data loss and accuracy
+
+    test_losses = [] # track loss
+    num_correct = 0
+
+    # init hidden state
+    h = net.init_hidden(batch_size)
+
+    net.eval()
+    # iterate over test data
+    for inputs, labels in test_loader:
+
+        # Creating new variables for the hidden state, otherwise
+        # we'd backprop through the entire training history
+        h = tuple([each.data for each in h])
+
+        # get predicted outputs
+        output, h = net(inputs, h)
+
+        # calculate loss
+        test_loss = criterion(output.squeeze(), labels.float())
+        test_losses.append(test_loss.item())
+
+        # convert output probabilities to predicted class (0 or 1)
+        pred = torch.round(output.squeeze())  # rounds to the nearest integer
+
+        # compare predictions to true label
+        correct_tensor = pred.eq(labels.float().view_as(pred))
+        correct = np.squeeze(correct_tensor.numpy())
+        np.squeeze(correct_tensor.cpu().numpy())
+        num_correct += np.sum(correct)
+
+    # -- stats! -- ##
+    # avg test loss
+    print("Test loss: {:.3f}".format(np.mean(test_losses)))
+
+    # accuracy over all test data
+    test_acc = num_correct/len(test_loader.dataset)
+    print("Test accuracy: {:.3f}".format(test_acc))
+
+
